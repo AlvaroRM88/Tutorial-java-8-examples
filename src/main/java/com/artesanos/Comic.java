073f168f87e6e6c5513212b9e9f3251ac943a5a1
AlvaroRM88 <arm071088@gmail.com>
2018-06-17 13:16:57 +0200

    Using try/catch to handle the npe

--- src/main/java/com/artesanos/Comic.java
+++ src/main/java/com/artesanos/Comic.java
@@ -6,19 +6,19 @@ public class Comic {
 	 * Check if the comic is about Spiderman without checking if the argument is null.
 	 * Bad practice in general.
 	 */
-	private static boolean isComicSpiderman(String collection) { 
+	private static Boolean isComicSpiderman(String collection) { 
 		
 		return collection.contains("Spiderman");
 		
 	}
 	
 	/*
-	 * Check if the comic is about Ironman without checking if the argument is null.
-	 * Good practice, but without using the functionality of Optional
+	 * Check if the comic is about Ironman checking if the argument is null.
+	 * Good practice but without using the functionality of Optional
 	 */
-	private static boolean isComicIronman(String collection) {
+	private static Boolean isComicIronman(String collection) {
 		
-		boolean isComicIronman = false;
+		Boolean isComicIronman = false;
 		
 		if(collection != null) 
 			isComicIronman = collection.contains("Ironman");
@@ -27,18 +27,39 @@ public class Comic {
 		
 	}
 	
+	/*
+	 * Check if the comic is about Thor catching the exception
+	 * Good practice depend of the circumstances
+	 */
+	private static Boolean isComicThor(String collection) {
+		
+		Boolean isComicThor = false;
+		
+		try {
+			
+			isComicThor = collection.contains("Thor");
+			
+		} catch(NullPointerException npe) {
+			
+			System.out.println("Catching the exception " + npe.getMessage());
+			
+		}
+		
+		return isComicThor;
+		
+	}
+	
 
 	public static void main(String[] args) {
 		
-		String stringNull = null;
+		String collection = null;
 		
-		System.out.println("Is comic Ironman: " + isComicIronman(stringNull));
+		System.out.println("Is comic about Thor: " + isComicThor(collection));
 		
-		System.out.println("Is comic Spiderman: " + isComicSpiderman(stringNull));
+		System.out.println("Is comic about Ironman: " + isComicIronman(collection));
 		
+		System.out.println("Is comic abotu Spiderman: " + isComicSpiderman(collection));
 		
-		
-
 	}
 
 }
